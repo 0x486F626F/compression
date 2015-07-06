bda1c1414db761486e9009055c5737dd8afe37b3
acgatea <acgatea@uwaterloo.ca>
2015-07-06 16:37:17 -0400

    add folder encode

---
+++ encode/decode.h
@@ -0,0 +1,8 @@
+#ifndef __DECODE_H__
+#define __DECODE_H__
+#include <string>
+#include <iostream>
+
+std::string decodeText(std::istringstream & in);
+
+#endif
---
+++ encode/encode.h
@@ -0,0 +1,7 @@
+#ifndef __ENCODE_H__
+#define __ENCODE_H__
+#include <string>
+
+std::string bestCompression (std::string text);
+
+#endif
